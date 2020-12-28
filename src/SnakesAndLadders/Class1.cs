ae291ef6b5ffabed51f804a5ceb941d0e22a6c36
Tejas Shinde <tshinde@tavisca.com>
2020-12-28 10:33:51 +0530

    [Mod] Initial folder structure

---
+++ src/SnakesAndLadders/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakesAndLadders
+{
+    public static class RandomGenerator
+    {
+        private static readonly Random _random = new Random();
+        private static readonly int _min = 1;
+        private static readonly int _max = 6;
+
+        public static int GetRandomDiceNumber()
+        {
+            return _random.Next(_min, _max); 
+        }
+    }
+}
